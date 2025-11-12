287bb1a0559d1ea727628a9689b41e16b6c1c4fc
juanqinliu <3220235001@bit.edu.cn>
2025-11-12 21:53:02 +0800

    logo imshow

--- src/App.tsx
+++ src/App.tsx
@@ -68,7 +68,7 @@ function App() {
           <div className="flex justify-between items-center">
             <div className="flex items-center gap-2">
               <div className="relative">
-                <img src="/Logo.png" alt="EnglishForge" className="w-16 h-16 object-contain" />
+                <img src="./Logo.png" alt="EnglishForge" className="w-16 h-16 object-contain" />
               </div>
               <div>
                 <h1 className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
